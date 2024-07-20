0865c394a700abeca5f3dbe36c3e89a5db8b8783
pravesh yadav <proyadavji07@gmail.com>
2024-07-20 17:44:51 +0530

    connect to data base

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to MongoDB!');
+    }).catch((err) => {
+        console.log(err);
+});
 
 const app = express();
 
-app.listen(300,( ) => {
-    console.log('Server is running on port 3000!');
+app.listen(3000,( ) => {
+    console.log('Server is running on port 3000');
     }
 );
