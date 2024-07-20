e4684946eaafc74a4eff8623fd1de653691472bd
pravesh yadav <proyadavji07@gmail.com>
2024-07-20 18:08:32 +0530

    create user model

---
+++ api/models/useer.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        reruired: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        reruired: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        reruired: true,
+    }
+},{timestamps: true });
+
+const User = mongoose.model{'User', userSchema};
+
+export default User;
